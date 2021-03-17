0b582fe81b59e60a42e0ae6531a4470fbc173b0a
Daniel Axtens <dja@axtens.net>
2021-03-18 00:42:50 +1100

    make ccan config.h just include include/config.h
    Signed-off-by: Daniel Axtens <dja@axtens.net>

--- ccan/config.h
+++ ccan/config.h
@@ -13,18 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/* Dummy config.h for CCAN test suite */
+/* Dummy config.h for CCAN test suite - defer all to our main config.h  */
 
-#define HAVE_BUILTIN_TYPES_COMPATIBLE_P 1
-#define HAVE_TYPEOF 1
-
-#ifndef HAVE_BIG_ENDIAN
-#define HAVE_BIG_ENDIAN 0
-#endif
-
-#ifndef HAVE_LITTLE_ENDIAN
-#define HAVE_LITTLE_ENDIAN 0
-#endif
-
-#define HAVE_BYTESWAP_H 0
-#define HAVE_BSWAP_64 0
+#include <config.h>
